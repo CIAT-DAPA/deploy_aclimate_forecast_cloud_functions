9eebfd57786ad862f3b83dc7fd0b7e88a60761b3
santiagoC <s.calderon@cgiar.org>
2025-02-26 16:30:48 -0500

    secrest

--- start-aclimate-instance/main.py
+++ start-aclimate-instance/main.py
@@ -1,19 +1,10 @@
 import googleapiclient.discovery
-from google.cloud import secretmanager
-
-def access_secret(secret_name):
-    client = secretmanager.SecretManagerServiceClient()
-    project_id = "my-gcp-project"  # Reemplaza con tu Project ID
-
-    secret_path = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
-    response = client.access_secret_version(name=secret_path)
-    
-    return response.payload.data.decode("UTF-8")
+import os
 
 def start_compute_instance(event, context):
-    project = access_secret("PROJECT_ID")
-    zone = access_secret("ZONE")
-    instance_name = access_secret("INSTANCE_NAME")
+    project = os.getenv("PROJECT_ID")
+    zone = os.getenv("ZONE")
+    instance_name = os.getenv("INSTANCE_NAME")
 
     compute = googleapiclient.discovery.build("compute", "v1")
 
